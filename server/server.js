7f8cb14cdb80a5aa9feb47a53139c04d093d7a9e
Hannah <hfhealy@gmail.com>
2022-10-03 14:29:30 -0700

    clean up code

--- server/server.js
+++ server/server.js
@@ -6,12 +6,4 @@ const app = express();
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
-// app.get('/api/:sign', (req, res) => {
-//     axios
-//     // .get(`https://ohmanda.com/api/horoscope/${req.params.sign}`)
-//     .get(`https://aztro.sameerkumar.website/?sign=${req.params.sign}&day=today`)
-
-//     .then(zodiac => res.send(zodiac.data.horoscope))
-// });
-
 module.exports = app;
--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 //horoscope app using a horoscope API and mercury retrograde API
 //display dropdown menu to choose your astrological sign
 //daily horoscope populates according to your sign
-//mercury retrograde also populates with tips for true or false
-//image populates for each sign after clicking button 
+//meme image populates for each sign after clicking button 
+//mercury retrograde for current date populates
 
 import React, { Component } from 'react';
 import axios from 'axios';
@@ -10,6 +10,7 @@ import axios from 'axios';
 class App extends Component {
     constructor(props){
         super(props);
+        //sets default state
         this.state = {
             json: [],
             mercury: '',
@@ -21,93 +22,56 @@ class App extends Component {
             month: '',
             day: ''
         }
+        //binds functions
         this.handleClick = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
-
-
-    
-    
-
+    //handleChange function sets state according to dropdown selection of zodiac sign
     handleChange(event) {
         this.setState({[event.target.name]: event.target.value});
-        
-        console.log(this.state.sign, event.target.value)
     }
 
+    //handleClick function runs after 'get horoscope' button is clicked
     handleClick(event) {
-        var that = this;
-        
+            
+        //Grab API data for sign selected from dropdown menu and set state to json variable
             const URL = `https://aztro.sameerkumar.website/?sign=${this.state.sign}&day=today`;
             fetch(URL, {
                 method: 'POST'
             }).then(response => response.json())
             .then(json => { this.setState({json}); });
         
-        // axios
-            
-            // .get(`/api/${this.state.sign}`) 
-            // .get(`https://aztro.sameerkumar.website/?sign=${this.state.sign}&day=today`) 
+        //create new date for current day
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+            var yyyy = today.getFullYear();
+        //format today variable with year first to get mercury retrograde API data for current date
+            today = yyyy + '-' + mm + '-' + dd;
+        //set state for year, month and day 
+            this.setState({year: yyyy, month: mm, day:dd})
         
-
-            // .then(response => response.data)
-
-            
-            // .then(json => that.setState({output: json}));
-
-        // JSON.parse(JSON.stringify(this.state.json));
-        // this.setState({output: this.state.json.horoscope})
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); 
-        var yyyy = today.getFullYear();
-        today = yyyy + '-' + mm + '-' + dd;
-        console.log("today", today);
-        this.setState({year: yyyy, month: mm, day:dd})
+        //fetch mercury retrograde API data for today's date
+        //set boolean variable to true or false depending on API response data for current date
+        //set state for mercury to boolean variable
         axios
-            
             .get(`https://mercuryretrogradeapi.com?date=${today}`)
-            
+
             .then(response => {
-                console.log(response.data.is_retrograde); 
                 let boolean = response.data.is_retrograde == true ? "true" : "false";
-                that.setState({mercury: boolean})
+                this.setState({mercury: boolean})
             })
 
-            this.setState({image: this.state.sign});
-
-
-            
+            this.setState({image: this.state.sign});      
     }
-
-   
-
-    // render() {
-    //     return (
-    //       <div>
-    //           Current Date: {this.state.json.current_date} <br />
-    //           Compatibility: {this.state.json.compatibility} <br />
-    //           Lucky Number: {this.state.json.lucky_number} <br />
-    //           Lucky Time: {this.state.json.lucky_time} <br />
-    //           Color: {this.state.json.color} <br />
-    //           Date Range: {this.state.json.date_range} <br />
-    //           Mood: {this.state.json.mood} <br />
-    //           Description: {this.state.json.description} <br />
-    //       </div>
-    //     );
-    // }
-
 
     render() {
         return (
-        
-            <div>
+    <div>
         <img src="https://horoscope-react.s3.us-west-1.amazonaws.com/Daily+(1).png" id="headerimg" className="img-fluid w-75"/>
         <div className="row mx-auto d-block w-75">
-        <div className= "col">
-        
-        {/* <h1 className='text-center'>Daily Horoscope</h1> */}
-        <p className="form-label text-center" id="rec">Choose Your Zodiac Sign...</p>
+            <div className= "col">
+                <p className="form-label text-center" id="rec">Choose Your Zodiac Sign...</p>
                 <select className="form-select" name="sign" onChange={this.handleChange} value={this.state.sign}>
                     <option>Select Your Sign</option>
                     <option name="aquarius" value="aquarius">Aquarius</option>
@@ -124,11 +88,10 @@ class App extends Component {
                     <option name="capricorn" value="capricorn">Capricorn</option>
                 </select>
                 <button className="w-100" type="button" name="button" onClick={this.handleClick}>Get Horoscope</button>
-                </div>
-                </div>
-                <div id="content">
-                <div className="row row-cols-1 g-2 g-lg-3">
-                {/* <img src={require('./images/aquarius.png')} onClick={this.handleClick} /> */}
+            </div>
+        </div>
+        <div id="content">
+            <div className="row row-cols-1 g-2 g-lg-3">
                 <div className="col" id="image-col">
                 {this.state.image == "" ? <span></span> : <img src={`https://horoscope-react.s3.us-west-1.amazonaws.com/${this.state.image}.jpeg`} onClick={this.handleClick} name="image" className='img-fluid w-50 mx-auto d-block'/>}
                 </div>
@@ -138,7 +101,7 @@ class App extends Component {
                 <div id="output" name="output" className="text-center" onClick={this.handleClick}>{this.state.json.description}
                 </div>
                 </div>
-                </div>
+            </div>
                 <div id="mercury" name="mercury" onClick={this.handleClick}>{this.state.mercury == "" ? <span></span> 
                 : this.state.mercury == "true" ? 
                 <div>
@@ -150,9 +113,9 @@ class App extends Component {
                 <p className="text-center">No, mercury is not in retrograde today! Resume life as normal.</p>
                 </div>
                 }
-                </div>
-                </div>
+            </div>
         </div>
+    </div>
         );
     }
 }
